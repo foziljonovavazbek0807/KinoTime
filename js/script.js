a2f79632cf007a868a4408b062c4c4ed22697e81
Avazbek <foziljonovavazbek0807@gmail.com>
2022-02-10 00:08:41 +0500

    final main page

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // aos
 AOS.init();
+
+// first swiper
+let i = 0;
+
+$('.slide').click(function() {
+    $('.active').addClass('nonActive').find('span').empty()
+    $(this).removeClass('nonActive').addClass('active')
+    i = $(this).index()
+})
+
+setItner = setInterval(go, 4000);
+
+function go() {
+    $('.active').addClass('nonActive').find('span').empty()
+    $($('.slide')[i % 10]).removeClass('nonActive').addClass('active')
+    i++;
+}
