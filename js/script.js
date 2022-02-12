91750a6dd6efcfc1fd398e02902bc302b0170510
Avazbek <foziljonovavazbek0807@gmail.com>
2022-02-12 15:22:15 +0500

    final.., present

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-// aos
-AOS.init();
-
-
 // big swiper
 
-let Swiper = 0;
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     loop: true,
     autoplay: {
-        delay: 4000,
+        delay: 5000,
         disableOnInteraction: false,
     },
     pagination: {
