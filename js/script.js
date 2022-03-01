79b71956b0d900f8183e9a6bbe21fc86597fa727
Avazbek <foziljonovavazbek0807@gmail.com>
2022-03-01 12:52:18 +0500

    final one step

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 // big swiper
 
-
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
+    effect: "cards",
+    grabCursor: true,
     autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
+      delay: 2500,
+      disableOnInteraction: false,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+    loop: true,
+  });
